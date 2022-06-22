b20d25f49846f7552f7c20525c2c86bc3f0891f9
Gianmarco G <giangarof98@gmail.com>
2022-06-22 18:26:44 -0400

    fetch data

--- src/components/header.vue
+++ src/components/header.vue
@@ -1,7 +1,8 @@
 <template>
-  <div class="text-center">
-    Covid-19 Tracker
-  </div>
+  <header class="text-center bg-blue-800 text-white p-4 mb-10">
+    <div class="text-3xl md:text-5xl font-bold mb-3">Covid-19 Tracker</div>
+    <p>API by <a href="https://covid19api.com" target="_blank" class="text-blue-300">covid19api.com</a></p>
+  </header>
 </template>
 
 <script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -8,8 +8,17 @@
 
 export default {
   name: 'Home',
-  components: {
-  
+  components: {},
+  methods: {
+    async fetchData(){
+      const res = await fetch('https://api.covid19api.com/summary');
+      const data = await res.json();
+      return data;
+    }
+  },
+  async created(){
+    const data = await this.fetchData();
+    console.log(data)
   }
 }
 </script>
